2527c9a54d237b130189999bf6aa08b00de83588
ZZZubec <zzzubec@pisem.net>
2026-08-07 13:37:17 +0500

    add driver light
    small fix
    
    small fix 2

--- Core/Src/Backlight.c
+++ Core/Src/Backlight.c
@@ -7,6 +7,7 @@
 
 
 #include "Backlight.h"
+#include "PCF8575.h"
 
 uint8_t lightWork_ = BL_WORK_FAILURE;
 uint8_t lightMode_ = BL_AUTO_OPERATION_MODE;
@@ -16,6 +17,47 @@ uint16_t needLightLevel_ = 0;
 TIM_HandleTypeDef* lightTimer;
 I2C_HandleTypeDef* backLightHi2c2;
 
+//light
+//uint8_t P0[2] = {0X01, 0x00};
+uint8_t P0[2] = {0X00, 0x00};
+uint8_t P1[2] = {0X02, 0x00};
+
+uint8_t P2[2] = {0X04, 0x00};
+uint8_t P3[2] = {0X08, 0x00};
+
+uint8_t P4[2] = {0X10, 0x00};
+uint8_t P5[2] = {0X20, 0x00};
+
+uint8_t P6[2] = {0X40, 0x00};
+uint8_t P7[2] = {0X80, 0x00};
+
+uint8_t P8 [2] = {0X00, 0x01};
+uint8_t P9 [2] = {0X00, 0x02};
+
+uint8_t P10[2] = {0X00, 0x04};
+uint8_t P11[2] = {0X00, 0x08};
+
+uint8_t P12[2] = {0X00, 0x10};
+uint8_t P13[2] = {0X00, 0x20};
+
+uint8_t P14[2] = {0X00, 0x40};
+uint8_t P15[2] = {0X00, 0x80};
+
+uint8_t B1_W[2] = {0X00, 0x40};
+uint8_t B1_G[2] = {0X00, 0xC0};
+
+uint8_t B2_W[2] = {0X00, 0x10};
+uint8_t B2_G[2] = {0X00, 0x30};
+
+uint8_t B3_W[2] = {0X00, 0x04};
+uint8_t B3_G[2] = {0X00, 0x0C};
+
+uint8_t B4_W[2] = {0X00, 0x35};
+uint8_t B4_G[2] = {0X00, 0x36};
+
+uint8_t B6_W[2] = {0X10, 0x00};
+uint8_t B6_G[2] = {0X30, 0x00};
+
 // Возвращает 1 если датчик отвечает, 0 если нет
 uint8_t BH1780_CheckPresence(void) {
 	return (HAL_I2C_IsDeviceReady(&backLightHi2c2, BH1780_ADDR_WRITE, 2, 100) == HAL_OK);
@@ -24,12 +66,20 @@ uint8_t BH1780_CheckPresence(void) {
 // Инициализация модуля при старте
 void BacklightInit(I2C_HandleTypeDef* hi2c2, TIM_HandleTypeDef* htim3)
 {
+	//hi2c
 	backLightHi2c2 = hi2c;
-	lightTimer = htim3;
 	if(BH1780_CheckPresence() > 0)
 	{
 		lightWork_ = BL_WORK_OK;
 	}
+
+	//TIM3
+	lightTimer = htim3;
+	HAL_TIM_PWM_Start_IT(lightTimer, TIM_CHANNEL_1);
+
+	PCF8575_Init(0x40, 100);
+	PCF8575_Reset(&hi2c3);
+
 }
 
 // Текущий признак исправности
@@ -43,22 +93,27 @@ void BacklightUpdate()
 {
 	if(lightMode_ == BL_AUTO_OPERATION_MODE)
 	{
-		if(lightLevel_ > needLightLevel_)
+		//HAL_ADC_Start(&hadc1); // Запуск
+		if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) { // Ожидание (таймаут 10мс)
+			brightness_ = HAL_ADC_GetValue(&hadc1); // Считываем результат
+			//needLightLevel_ = ?
+		}
+		if(TIM1->CCR1 > needLightLevel_)
 		{
-			if(lightLevel_ + 100 > needLightLevel_)
+			if(TIM1->CCR1 + 100 > needLightLevel_)
 			{
-				OnBacklightChangeLightLevel(lightLevel_ + 100);
+				OnBacklightChangeLightLevel(TIM1->CCR1 + 100);
 			}
 			else
 			{
 				OnBacklightChangeLightLevel(needLightLevel_);
 			}
 		}
-		else if(lightLevel_ < needLightLevel_)
+		else if(TIM1->CCR1 < needLightLevel_)
 		{
-			if(lightLevel_ - 100 < needLightLevel_)
+			if(TIM1->CCR1 - 100 < needLightLevel_)
 			{
-				OnBacklightChangeLightLevel(lightLevel_ - 100);
+				OnBacklightChangeLightLevel(TIM1->CCR1 - 100);
 			}
 			else
 			{
@@ -92,13 +147,14 @@ uint16_t BacklightGetBrightness()
 // Текущий уровень подсветки
 uint16_t BacklightGetLightLevel()
 {
-	return lightLevel_;
+	return TIM1->CCR1;
 }
 
 void OnBacklightChangeLightLevel(uint16_t light)
 {
-	lightLevel_ = light;
-	//TIM->CCR = lightLevel_
+	TIM1->CCR1 = light;
+	HAL_Delay(100);
+	PCF8575_write(&hi2c3, B1_W);
 }
 
 void BacklightSetLightLevel(uint16_t light)
